0e0a093e1fd81c5456668cc99dd33a112acebad9
Zachary Blackwood <zachary@streamlit.io>
2022-08-17 10:24:12 -0400

    Add from_markdown method

--- stoc.py
+++ stoc.py
@@ -9,23 +9,24 @@ a.toc {
 }
 </style>"""
 
-    
+
 class stoc:
     def __init__(self):
         self.toc_items = list()
 
-    def h1(self, text):
-        st.write(f"# {text}")
+    def h1(self, text: str, write: bool = True):
+        if write:
+            st.write(f"# {text}")
         self.toc_items.append(("h1", text))
 
-
-    def h2(self, text):
-        st.write(f"## {text}")
+    def h2(self, text: str, write: bool = True):
+        if write:
+            st.write(f"## {text}")
         self.toc_items.append(("h2", text))
 
-
-    def h3(self, text):
-        st.write(f"### {text}")
+    def h3(self, text: str, write: bool = True):
+        if write:
+            st.write(f"### {text}")
         self.toc_items.append(("h3", text))
 
     def toc(self):
@@ -34,8 +35,26 @@ class stoc:
         markdown_toc = ""
         for title_size, title in self.toc_items:
             h = int(title_size.replace("h", ""))
-            markdown_toc += " " * 2 * h + "- " + f'<a href="#{normalize(title)}" class="toc"> {title}</a> \n'
+            markdown_toc += (
+                " " * 2 * h
+                + "- "
+                + f'<a href="#{normalize(title)}" class="toc"> {title}</a> \n'
+            )
         st.sidebar.write(markdown_toc, unsafe_allow_html=True)
+
+    @classmethod
+    def from_markdown(cls, text: str):
+        self = cls()
+        for line in text.splitlines():
+            if line.startswith("###"):
+                self.h3(line[3:], write=False)
+            elif line.startswith("##"):
+                self.h2(line[2:], write=False)
+            elif line.startswith("#"):
+                self.h1(line[1:], write=False)
+        st.write(text)
+        self.toc()
+
 
 
 def normalize(s):
